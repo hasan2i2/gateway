ac6dcc8a7ad3f128f0c99e84ba932812928b8923
hassanamjad <hassan@hugenet.ir>
2020-03-13 19:10:28 +0330

    added user_id

--- migrations/2016_05_02_193213_create_gateway_transactions_table.php
+++ migrations/2016_05_02_193213_create_gateway_transactions_table.php
@@ -8,59 +8,61 @@ use hugenet\Gateway\Enum;
 
 class CreateGatewayTransactionsTable extends Migration
 {
-	function getTable()
-	{
-		return config('gateway.table', 'gateway_transactions');
-	}
+    function getTable()
+    {
+        return config('gateway.table', 'gateway_transactions');
+    }
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create($this->getTable(), function (Blueprint $table) {
-			$table->engine = "innoDB";
-			$table->unsignedBigInteger('id', true);
-			$table->enum('port', [
-				Enum::MELLAT,
-				Enum::JAHANPAY,
-				Enum::PARSIAN,
-				Enum::PASARGAD,
-				Enum::PAYLINE,
-				Enum::SADAD,
-				Enum::ZARINPAL,
-				Enum::SAMAN,
-				Enum::ASANPARDAKHT,
-				Enum::PAYPAL,
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->getTable(), function (Blueprint $table) {
+            $table->engine = "innoDB";
+            $table->unsignedBigInteger('id', true);
+            $table->enum('port', [
+                Enum::MELLAT,
+                Enum::JAHANPAY,
+                Enum::PARSIAN,
+                Enum::PASARGAD,
+                Enum::PAYLINE,
+                Enum::SADAD,
+                Enum::ZARINPAL,
+                Enum::SAMAN,
+                Enum::ASANPARDAKHT,
+                Enum::PAYPAL,
                 Enum::IRANKISH,
                 Enum::SADERAT,
                 Enum::PAYIR
-			]);
-			$table->decimal('price', 15, 0);
-			$table->string('ref_id', 100)->nullable();
-			$table->string('tracking_code', 50)->nullable();
-			$table->string('card_number', 50)->nullable();
-			$table->enum('status', [
-				Enum::TRANSACTION_INIT,
-				Enum::TRANSACTION_SUCCEED,
-				Enum::TRANSACTION_FAILED,
-			])->default(Enum::TRANSACTION_INIT);
-			$table->string('ip', 20)->nullable();
-			$table->timestamp('payment_date')->nullable();
-			$table->nullableTimestamps();
-			$table->softDeletes();
-		});
-	}
+            ]);
+            $table->decimal('price', 15, 0);
+            $table->string('ref_id', 100)->nullable();
+            $table->string('tracking_code', 50)->nullable();
+            $table->string('card_number', 50)->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->enum('status', [
+                Enum::TRANSACTION_INIT,
+                Enum::TRANSACTION_SUCCEED,
+                Enum::TRANSACTION_FAILED,
+            ])->default(Enum::TRANSACTION_INIT);
+            $table->string('ip', 20)->nullable();
+            $table->timestamp('payment_date')->nullable();
+            $table->nullableTimestamps();
+            $table->softDeletes();
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop($this->getTable());
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop($this->getTable());
+    }
 }
